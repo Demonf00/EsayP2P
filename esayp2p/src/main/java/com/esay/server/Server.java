e5b093bb8cae40bfa6da5acaf044a19b092b8116
Jin Che <bruceche608@gmail.com>
2023-12-12 19:55:44 -0500

    Server init

--- esayp2p/src/main/java/com/esay/server/Server.java
+++ esayp2p/src/main/java/com/esay/server/Server.java
@@ -1,4 +1,7 @@
 package com.esay.server;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.util.Scanner;
 import java.net.Socket;
@@ -7,23 +10,49 @@ public class Server extends Thread{
 
     Scanner inputScanner;
     public int port;
-    public boolean running;
+    private boolean running;
     public ServerSocket serverSocket;
 
+    public Server () {
+        this.port = 5968;
+    }
     public Server (int port) {
         this.port = port;
     }
 
     public void startServer() {
         try {
-            serverSocket = new ServerSocket(port);
             this.start();
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
 
-    public void run() {
+    public void shutServer () {
+        this.running = false;
+    }
 
+    public void run () {
+        try {
+            serverSocket = new ServerSocket(port);
+            this.running = true;
+            while (!this.running) {
+                System.out.println("Waiting for the client request");
+                Socket socket = serverSocket.accept();
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                String message = (String) ois.readObject();
+                System.out.println("Message Received: " + message);
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                oos.writeObject("Hi Client "+message);
+                ois.close();
+                oos.close();
+                socket.close();
+                if(message.equalsIgnoreCase("exit")) break;
+            }
+            System.out.println("Shutting down Socket server!!");
+            serverSocket.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 }
